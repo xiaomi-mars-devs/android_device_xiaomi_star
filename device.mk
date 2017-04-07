6e0373c84e2ca1f409001a518840704661241376
Glen Kuhne <kuh@google.com>
2017-04-07 14:54:17 -0700

    star: Set default hotspot ssid
    Added a device specific overlay that changes the default name
    from "AndroidAp" to customized name
    
    Test: Builds, manually verified name change
    Bug: 36368950
    Change-Id: I38a5f6fb950d6a45342080bae186a8ed276d94bf

--- device.mk
+++ device.mk
@@ -36,5 +36,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# WiFi
+PRODUCT_PACKAGES += \
+    TargetWifiOverlay
+
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/star/star-vendor.mk)
