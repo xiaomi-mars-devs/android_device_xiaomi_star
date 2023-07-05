9e0098bc1ce5f06f1f527d3a8cdece5f81474f08
FlowerSea0208 <zzp060208@gmail.com>
2023-07-05 19:12:24 +0800

    star: Initial device tree
    Change-Id: I5f6bcc1b8d9a0fa8b2c1bf164ef580e1dc2c875c

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_mars.mk \
+    $(LOCAL_DIR)/lineage_star.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_mars-userdebug \
+    lineage_star-userdebug
